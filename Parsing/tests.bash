4f33f51995c50687b3ef804dc41b904096f5ecfa
alanjeanpierre <ajeanpierre825@gmail.com>
2018-02-20 00:37:45 -0800

    Add test script for parser

---
+++ Parsing/tests.bash
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+#make
+
+long=false
+if [ "$1" = "-l" ]
+then    
+    long=true
+fi
+
+pass=0
+tests=0
+
+
+flag=false
+
+for file in samples/*.decaf
+do 
+    tests=$((tests + 1))
+    out="samples/$(basename $file .decaf).out"
+    echo -e "**********************************"
+    echo -e "*** Running $file ***"
+    echo -e "**********************************"
+
+    if [ "$long" = true ]
+    then
+        cat "$file"
+        echo -e "\n**********************************"
+    fi
+    d="$(./dcc < $file 2>&1 | diff - $out 2>&1)"
+    if [ "$d" != "" ]
+    then
+        echo -e "\e[91mTest fail\e[39m"
+        echo ""
+        if [ "$long" = true ]
+        then
+            echo -e "\e[31m$d\e[39m"
+        fi
+        flag=true      
+    else
+        if [ "$long" = true ]
+        then
+            cat "$out"
+            echo -e "\n**********************************"
+        fi
+        echo -e "\e[92mTest pass\e[39m"  
+        pass=$((pass + 1))
+
+    fi
+    echo ""
+    echo ""
+done
+    
+if [ "$flag" = "true" ]
+then
+    echo -e "\e[91m***************************"
+    echo "$pass / $tests Tests Passed"
+    echo "***************************"
+else
+    echo -e "\e[92m***************************"
+    echo "$pass / $tests Tests Passed"
+    echo "***************************"
+fi
